52fdc960217bfc2473b036a618a7338dcfd0ea34
Piyush Kumar Singh <orgpiyush858@gmail.com>
2025-11-13 18:20:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9393599555-fb506",
+  "appId": "1:812389858204:web:786ae5c0f5220fd3fa2659",
+  "apiKey": "AIzaSyDRCzyqUuDKmanbEbzSbHC8zr4cPv4OEyc",
+  "authDomain": "studio-9393599555-fb506.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "812389858204"
+};
